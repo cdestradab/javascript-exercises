a27d55f5f6a5b910c5696b4527bc3f7b58a479f1
Cristian Estrada <59891802+cdestradab@users.noreply.github.com>
2024-01-18 02:51:27 -0500

    Solve 04_removeFromArray exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+  let newArray = array.slice();
 
+  for (let arg of args) {
+    const myIndex = newArray.findIndex((value, index, array) => value === arg);
+
+    if (myIndex === -1) { continue; }
+
+    newArray.splice(myIndex,1);
+  }
+
+  return newArray;
 };
 
 // Do not edit below this line
