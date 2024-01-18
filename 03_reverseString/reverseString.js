1b4b85b30f1c77eff2217849e2a4fd6829ca7b3b
Cristian Estrada <59891802+cdestradab@users.noreply.github.com>
2024-01-18 01:41:58 -0500

    Solve 03_reverseString exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,17 @@
-const reverseString = function() {
+const reverseString = function(text) {
+  newText = [];
 
+  if (text === "") {
+    return "";
+  } else {
+
+    let i = text.length;
+    for (i; i >= 0; i--) {
+      newText.push(text[i]);
+    }
+
+    return newText.join("");
+  }
 };
 
 // Do not edit below this line
