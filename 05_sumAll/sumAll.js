7f827539b17e4bccd2e0788cbf8bb4391c876693
Cristian Estrada <59891802+cdestradab@users.noreply.github.com>
2024-01-18 23:56:28 -0500

    Solve 05_sumAll exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,30 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+  //Catch wrong inputs as ERROR
+  if (num1 < 0 || num2 < 0) {
+    return "ERROR";
+  } else if (typeof num1 !== 'number' || typeof num2 !== 'number' ) {
+    return "ERROR";
+  }
 
+  let firstNumber;
+  let secondNumber;
+  //Orders the numbers by their value
+  if (num2 > num1) {
+    firstNumber = num1;
+    secondNumber = num2;
+  } else if (num1 > num2) {
+    firstNumber = num2;
+    secondNumber = num1;
+  } else {
+    return num1;
+  }
+
+  let totalSum = 0;
+  for (let i = firstNumber; i <= secondNumber; i++) {
+    totalSum += i;
+  }
+
+  return totalSum;
 };
 
 // Do not edit below this line
